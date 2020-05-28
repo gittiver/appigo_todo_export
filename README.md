b5e2f81dbea98323d21d51d000cd9646046361ff
Gulliver <gulliver@traumkristalle.net>
2020-05-28 15:31:17 +0200

    read projects as toplevel todo, attach tasks as child TODO items read simpler task types and attach them to parent projct or list decode dates decode recurrences

--- README.md
+++ README.md
@@ -10,9 +10,55 @@ select name,type,type_data,recurrence,advanced_recurrence from tasks where delet
 Currently all items are converted to TODO items with other columns as
 properties
 
+#Structure of Tasks table
+`
+CREATE TABLE tasks(task_id TEXT PRIMARY KEY,
+list_id TEXT,
+name TEXT,
+due_date DOUBLE,
+completion_date DOUBLE,
+priority INTEGER,
+note TEXT,
+mod_date DOUBLE,
+deleted INTEGER,
+dirty INTEGER,
+sync_id TEXT,
+local_sync_id TEXT,
+start_date DOUBLE,
+parent_sync_id TEXT,
+recurrence INTEGER,
+advanced_recurrence TEXT,
+context_id TEXT,
+tags TEXT,
+flags INTEGER,
+type INTEGER,
+type_data TEXT,
+parent_id TEXT,
+sort_order INTEGER,
+ds_due_date DOUBLE,
+ds_priority INTEGER,
+ps_due_date DOUBLE,
+ps_priority INTEGER,
+child_count INTEGER,
+time_zone_offset INTEGER,
+alert_dirty INTEGER,
+starred INTEGER,
+location_alert TEXT,
+expansion_properties TEXT,
+assigned_user_id TEXT,
+
+project_due_date DOUBLE,
+project_time_zone_offset INTEGER,
+project_start_date DOUBLE,
+project_priority INTEGER,
+project_starred INTEGER,
+start_date_time_zone_offset INTEGER,
+project_start_date_time_zone_offset INTEGER);
+
+`
 #TODO 
-* read projects as toplevel todo, attach tasks as child TODO items
-* read simpler task types and attach them to parent projct or list
+* read projects as toplevel todo, attach tasks as child TODO items (DONE)
+* read simpler task types and attach them to parent projct or list (DONE)
 * decode taskdata for tasktype 2
-* decode dates
-* decode recurrences
+* decode dates (DONE)
+* decode recurrences (DONE)
--- appigo_todo_export.py
+++ appigo_todo_export.py
@@ -1,51 +1,113 @@
-
+#!/usr/bin/python
 import sys
 import sqlite3
+import datetime
 
-class Converter(object):
-    """loads values from database and writes as org-mode formatted text to stdout"""
+tasktype = {
+    0:"task",
+    1: "project",
+    2: "contact",
+    6: "url"
+}
 
-    def load(self,dbname):
-        """opens database file with name dbname"""
+recurrence_string = {
+    0: "",
+    1: "+1w",
+    2: "+1m",
+    3: "+1y",
+    5: "+2w",
+    7: "+6m",
+    9: "",
+    50: "+1m",
+    101:"+1w",
+    105: "+2w",
+    150: "+3d",
+    103: "+1y",
+}
+def convert_tasks(dbname):
+        """
+        opens database file with name dbname
+        loads values from database and writes as org-mode formatted text to stdout
+        """
      
         connection = sqlite3.connect(dbname)
         cursor = connection.cursor()
-        cursor.execute("select name,type,type_data,recurrence,advanced_recurrence from tasks where deleted=0 and completion_date=-62135769600.0 order by parent_id") 
-        print("fetchall:")
+        cursor.execute("select name,type,type_data,recurrence,advanced_recurrence, task_id, parent_id, due_date, project_due_date, note from tasks where deleted=0 and completion_date=-62135769600.0 order by parent_id") 
         result = cursor.fetchall() 
+
+        children = {}
+        no_parent = []
         for r in result:
-            self.convert(r)
+            if r[6] is None:
+                no_parent.append(r)
+            elif r[6] ==u'':
+                no_parent.append(r)
+            else:
+                if r[6] in children:
+                    children[r[6]].append(r)
+                else:
+                    children[r[6]] =[r]
+                    
+        level=1               
+        for task in no_parent:
+            convert_task(task,level)
+            if task[5] in children:
+                for sub_task in children[task[5]]:
+                    convert_task(sub_task,level+1)
+                
         connection.close()
 
-    tasktype = {
-        0:"task",
-        1: "project",
-        2: "contact",
-        6: "url"
-        }
-       
-    def convert(self,row):
+def deadline(date,recurrence,advanced_recurrence):
+    if recurrence==0:
+        return "<"+date+ ">"
+        
+    else:
+        return "<"+ date +" " + recurrence_string[recurrence]+ ">"
+
+def double_to_date(d):
+    if d is None:
+        return ""
+    else:
+        return datetime.datetime.fromtimestamp(d).strftime("%Y-%m-%d %a %H:%M")
+
+def convert_task(row,level):
         """converts a single task row"""
   #      print row
-        print "** TODO ", row[0].encode('utf-8')
+        print level*'*',"TODO", row[0].encode('utf-8')
+        if row[1]==1:
+                if row[8]<64092211200.0:
+                        print "DEADLINE:", \
+                          deadline(double_to_date(row[8]),row[3],row[4])
+                
+        elif row[7]<64092211200.0:
+                print "DEADLINE:", \
+                  deadline(double_to_date(row[7]),row[3],row[4])
         print ":PROPERTIES:"
-        print ":Tasktype:"+Converter.tasktype[row[1]]
+        print ":Tasktype:",tasktype[row[1]]
         print ":type_data:", row[2].encode('utf-8')
         print ":recurrence:", row[3]
         print ":advanced_recurrence:", row[4]
-
+        print ":task_id:", row[5]
+        print ":parent_id:", row[6]
+        print ":due_date:", row[7]
         print ":END:"
-        print ""
+        
+        if row[9] !='':
+                print row[9].encode('utf8')
         
 def main(argv):
     if len(argv)<1:
-        inputfile ="x"
+        print "please call with inputfile name as first parameter"
+        sys.exit(1)
     else:
         inputfile = argv[0]
 
-    print inputfile
-    converter = Converter()
-    converter.load(inputfile)
+    print "#                  -*- mode: org -*-"
+    print "#+TITLE: Import from Appigo Todo"
+    print "#+OPTIONS: ^:{}"
+    print ""
+
+    convert_tasks(inputfile)
     
 if __name__ == "__main__":
    main(sys.argv[1:])
